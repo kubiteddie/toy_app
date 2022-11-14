edd905b47e73e42944566ed8f011248c3cc6997c
kubiteddie <eddiekubit0@gmail.com>
2022-11-13 19:03:13 -0500

    Added filters and associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  resources :users
+  root 'users#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
